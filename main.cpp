8421c4f4d89c36f6456d7fd4224cf2f09800ab29
Ivan <losevskiy.ivan@mail.ru>
2023-10-03 00:24:25 +1000

    Add function that allow user write text that will be writed in output.txt

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	string userText;
+	getline(cin, userText);
 	freopen("output.txt", "w", stdout);
 
-	cout << "hELlo WorLd$" << '\n';
+	cout << userText << '\n';
 	return 0;
 }
